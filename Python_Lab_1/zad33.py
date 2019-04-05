8180d2cea0abf6917e2298a7310bf3b146a8c4cf
kaczuchg711 <kaczatom4@gmail.com>
2019-04-05 13:54:21 +0200

    1.3 done without complex

--- Python_Lab_1/zad33.py
+++ Python_Lab_1/zad33.py
@@ -1,15 +1,23 @@
-import numpy
+import numpy as np
 
 def count_zero_of_a_function(a,b,c):
-    delta=a**2-4*b*c
+    delta=b**2-4*a*c
     if delta<0:
         print("brak miejsc zerowych rzeczywistych")
         return None
-
+    elif delta==0:
+        x0=-b/(2*a)
+        return  x0
+    else:
+        x1=(-b-np.sqrt(delta))/(2*a)
+        x2=(-b+np.sqrt(delta))/(2*a)
+        return x1,x2
 
 a=1
-b=1
-c=-2
+b=-2
+c=1
+
+print(count_zero_of_a_function(a,b,c))
 
 
 
