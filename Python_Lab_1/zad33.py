51299dfde39c75c7d087a6dc405d79658c61436a
kaczuchg711 <kaczatom4@gmail.com>
2019-04-05 13:15:27 +0200

    dev

--- Python_Lab_1/zad33.py
+++ Python_Lab_1/zad33.py
@@ -1,1 +1,15 @@
-print("no")
+import numpy
+
+def count_zero_of_a_function(a,b,c):
+    delta=a**2-4*b*c
+    if delta<0:
+        print("brak miejsc zerowych rzeczywistych")
+        return None
+
+
+a=1
+b=1
+c=-2
+
+
+
